e813cbda3d497a811883cbdb95291ce167cb7926
Furkan Ertekin <furkanertekin0@gmail.com>
2022-11-30 14:00:17 +0300

    first commit

---
+++ ForDongusu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForDongusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Faktöriyelini Hesaplamak İstediğiniz Sayıyı giriniz :");
+            string kullaniciDeger = Console.ReadLine();
+
+            int sayi1 = Convert.ToInt32(kullaniciDeger);
+            int faktoriyel = 1;
+
+            for (int i = sayi1; i >= 1; i--)
+            {
+                faktoriyel *= i;
+            }
+            Console.WriteLine(kullaniciDeger + " Değerinin Faktöriyelin sonucu : "+ faktoriyel  );
+
+            Console.ReadLine();
+        }
+    }
+}
